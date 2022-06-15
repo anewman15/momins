558681efec3f33fbf408564d0d1af3b0382af06a
Abdullah Numan <anewman15@hotmail.com>
2022-06-15 16:39:30 +0600

    Attach :photos to Gallery model, add columns

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,2 +1,6 @@
 class Gallery < ApplicationRecord
+  validates :name, presence: true, length: { in: 5..70 }
+  validates :description, presence: true, length: { in: 5..1500 }
+
+  has_many_attached :photos
 end
--- db/migrate/20220615092824_create_galleries.rb
+++ db/migrate/20220615092824_create_galleries.rb
@@ -1,8 +1,12 @@
 class CreateGalleries < ActiveRecord::Migration[6.1]
   def change
     create_table :galleries do |t|
+      t.string :name, null: false, default: ''
+      t.string :description, null: false, default: ''
 
       t.timestamps
     end
+
+    add_index :galleries, :name, unique: true
   end
 end
