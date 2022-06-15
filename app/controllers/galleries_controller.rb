bf1a8110de80089cd96ca975dd2de355af4f0b87
Abdullah Numan <anewman15@hotmail.com>
2022-06-15 18:11:37 +0600

    Install active_storage

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,2 +1,33 @@
 class GalleriesController < ApplicationController
+  def index
+    @galleries = Gallery.all
+  end
+
+  # def show
+  #  @gallery = Gallery.find_by(name: params[:name])
+  # end
+
+  # def create
+  #   @gallery = Gallery.new(gallery_params)
+  # end
+
+  def update
+    @gallery = Gallery.find_by(id: params[:id])
+
+    photo = params[:photo] if params[:photo]
+
+    if @gallery.exists?
+      @gallery.photos.attach(photo) if photo
+      render :photo_added
+    else
+      render json: { "status": 422, "message": 'Photo not added.' }, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def gallery_params
+    params.require(:gallery).permit(:name, :description)
+  end
 end
