9f2af7086fc6db07c6d842da34cc66f44c0ee782
Abdullah Numan <anewman15@hotmail.com>
2022-06-15 22:01:48 +0600

    Define, call getGalleryInfo func from Home comp

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import AddNewPhotoButton from './AddNewPhotoButton'
 import AddNewPhotoModal from './AddNewPhotoModal'
 
 const Home = () => {
+  const [galleryRequest, setGalleryRequest] = useState({
+  })
+
+  useEffect( () => {
+
+    const getGalleryInfo = () => {
+      try {
+        setGalleryRequest({
+          loading: true,
+        })
+        axios.get('/galleries')
+          .then(response => {
+            if (response.status == 200) {
+              const data = response.data
+              setGalleryRequest({
+                galleryData: data,
+              })
+            }
+          })
+      } catch (error) {
+        setGalleryRequest({
+          error: error.message,
+        })
+      }
+    }
+
+    getGalleryInfo()
+  }, [])
+
+  console.log(galleryRequest.galleryData?.gallery.photos)
   return (
     <div className="position-relative vh-100">
       <div className="">
-        fnjksjksflvjsjfjv
-        dcklsdjdvjns
-        xcvkxvn
-        xlknvklnfv jn
+        {
+          galleryRequest.loading && <span>Loading...</span>
+        }
       </div>
       <AddNewPhotoModal />
       <AddNewPhotoButton />
