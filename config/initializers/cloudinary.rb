d53f86216fe2d8237a3541cffdf61bb32ffe99b9
Abdullah Numan <anewman15@hotmail.com>
2022-06-16 00:39:08 +0600

    Create cloudinary config file

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'anewman15'
+  config.api_key = '616337668872956'
+  config.api_secret = '19Md4qDFc8Pu3N3_F69vXNBRquw'
+  config.secure = true
+  config.cdn_subdomain = true
+end
