a78a4ba872e3e3bd32c3c2fddeb2c3ee14f4ab49
Abdullah Numan <anewman15@hotmail.com>
2022-06-15 15:24:18 +0600

    Define Home comp

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import AddNewPhotoButton from './AddNewPhotoButton'
+import AddNewPhotoModal from './AddNewPhotoModal'
+
+const Home = () => {
+  return (
+    <div className="position-relative vh-100">
+      <div className="">
+        fnjksjksflvjsjfjv
+        dcklsdjdvjns
+        xcvkxvn
+        xlknvklnfv jn
+      </div>
+      <AddNewPhotoModal />
+      <AddNewPhotoButton />
+    </div>
+  )
+}
+
+export default Home
