150bb00b1bfbae958d6e4d2a29808435044b0314
Abdullah Numan <anewman15@hotmail.com>
2022-06-15 21:59:12 +0600

    Update seeds, routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :galleries, only: [ :index, :update]
+  resources :galleries, only: [ :index, :update], defaults: { format: :json }
 
   post 'presigned_url', to: 'direct_uploads#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Gallery.create(name: 'Momins', description: 'Moments of incessant creation. Inexhaustible creativity...')
+gallery = Gallery.create(name: 'Momins', description: 'Moments of incessant creation. Inexhaustible creativity...')
+10.times do |i|
+  gallery.photos.attach(io:
+    File.open("public/images/momins#{i}.jpeg"), filename: "momins#{i}.jpeg", content_type: 'image/jpeg')
+end
