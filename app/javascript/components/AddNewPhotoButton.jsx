432315c65d1226d408091964adf535650d00751a
Abdullah Numan <anewman15@hotmail.com>
2022-06-16 04:12:01 +0600

    Adjust css classes for App, btn comp

--- app/javascript/components/AddNewPhotoButton.jsx
+++ app/javascript/components/AddNewPhotoButton.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const AddNewPhotoButton = () => {
   return (
-  <div className="position-absolute bottom-0 end-0 py-5 mb-5">
-    <button
-      style={{ "fontSize": "3rem" }}
-      type="button"
-      className="bg-primary py-0 px-3 text-white border rounded-circle"
-      data-bs-toggle="modal"
-      data-bs-target="#upload-photo-modal"
-    >
-      +
-    </button>
-  </div>
+    <div className="position-fixed bottom-0 end-0 p-4">
+      <button
+        style={{ "fontSize": "3rem" }}
+        type="button"
+        className="bg-primary py-0 px-3 text-white border rounded-circle"
+        data-bs-toggle="modal"
+        data-bs-target="#upload-photo-modal"
+      >
+        +
+      </button>
+    </div>
   )
 }
 
--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 
 const App = () => {
   return (
-    <div className="container">
+    <div className="position-relative mx-2 px-2">
       <Navbar />
       <Home />
     </div>
